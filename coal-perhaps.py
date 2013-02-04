37de0ab4944c0351cd3ca7b770cf39be2caa8d3a
mzmcbride <g@mzmcbride.com>
2013-02-04 18:41:10 -0500

    Added coal-perhaps.py

---
+++ coal-perhaps.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# Public domain; MZMcBride; 2012
+
+import random
+
+from twitter.api import Twitter
+from twitter.oauth import OAuth, read_token_file
+
+import settings
+
+f = open(settings.initial_path + 'coal-wisdom.txt', 'r')
+wisdom = f.read().strip('\n').split('\n')
+f.close()
+
+oauth_filename = settings.initial_path + 'coal-oauth'
+oauth_token, oauth_token_secret = read_token_file(oauth_filename)
+
+poster = Twitter(auth=OAuth(oauth_token,
+                            oauth_token_secret,
+                            settings.consumer_key,
+                            settings.consumer_secret),
+                 secure=True,
+                 api_version='1',
+                 domain='api.twitter.com')
+
+# Twitter blocks duplicate posts, so try up to ten times.
+for i in range(10):
+    try:
+        poster.statuses.update(status=random.choice(wisdom))
+        break
+    except:  # Unnamed!
+        continue
