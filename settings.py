c4a4489bef94611d0091a44e932f79e530a9e920
mzmcbride <g@mzmcbride.com>
2013-02-04 18:50:42 -0500

    Added settings.py

---
+++ settings.py
@@ -0,0 +1,7 @@
+# Settings for Twitter bots
+
+initial_path = '/home/mzmcbride/scripts/twitter-bots/'
+
+# Shh. ;-)
+consumer_key = 'uS6hO2sV6tDKIOeVjhnFnQ'
+consumer_secret ='MEYTOS97VvlHX7K1rwHPEqVpTSqZ71HtvoK4sVuYk'
