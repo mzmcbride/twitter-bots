1715ac577b2ee8defc3e07fb3fb9e0270f529494
mzmcbride <g@mzmcbride.com>
2013-02-04 18:53:49 -0500

    Added dentist-visit.py

---
+++ dentist-visit.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# Public domain; MZMcBride; 2012
+
+import random
+
+from twitter.api import Twitter
+from twitter.oauth import OAuth, read_token_file
+
+import settings
+
+oauth_filename = settings.initial_path + 'dentist-oauth'
+oauth_token, oauth_token_secret = read_token_file(oauth_filename)
+
+poster = Twitter(auth=OAuth(oauth_token,
+                            oauth_token_secret,
+                            settings.consumer_key,
+                            settings.consumer_secret),
+                 secure=True,
+                 api_version='1',
+                 domain='api.twitter.com')
+
+# Twitter blocks duplicate posts, so try up to ten times.
+for i in range(10):
+    try:
+        poster.statuses.update(status='O'+random.randint(1,5)*'w'+'.')
+        break
+    except:  # Unnamed!
+        continue
